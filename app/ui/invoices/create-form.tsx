aaa8901d9f2d8ad5e0c292f210925ca0eb5c8911
AureLeeLi <aduynslaeger@gmail.com>
2024-05-20 23:26:38 +0200

    🚨 fixed createInvoice import

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -9,6 +9,7 @@ import {
 } from '@heroicons/react/24/outline';
 import { Button } from '@/app/ui/button';
 import { useFormState } from 'react-dom';
+import { createInvoice } from '@/app/lib/actions';
 
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
